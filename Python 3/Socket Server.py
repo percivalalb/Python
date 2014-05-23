8702bef1e1e45e67854dbd6b672c38f6b27e79ed
ProPercivalalb <propercivalalb@gmail.com>
2014-05-23 08:17:25 +0100

    Learning about sockets

---
+++ Python 3/Socket Server.py
@@ -0,0 +1,16 @@
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print('Got connection from', addr)
+   c.send(bytes('Thank you for connecting', 'UTF-8'))
+   c.close()                # Close the connection
+
+while True:
+    pass
